de2d39454e8392b3e221b03517da105355c6068a
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-09-23 18:36:08 +0200

    Testing model imports

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 import * as THREE from 'three';
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
@@ -18,11 +18,22 @@ camera.position.setZ( 30 );
 
 renderer.render( scene, camera );
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
-const torus = new THREE.Mesh( geometry, material );
+// const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+// const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
+// const torus = new THREE.Mesh( geometry, material );
 
-scene.add( torus );
+// scene.add( torus );
+
+const loader = new GLTFLoader();
+
+loader.load( 'shiba.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 
 const pointLight = new THREE.PointLight( 0xffffff ); 
 pointLight.position.set( 20, 20, 20 );
@@ -55,10 +66,6 @@ scene.background = spaceTexture;
 
 function animate() {
   requestAnimationFrame( animate );
-
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
 
   controls.update();
 
